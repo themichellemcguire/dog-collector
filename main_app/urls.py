8d1a65118d56984b7339bc7c7687990a1ba50b62
owner <themichellemcguire@gmail.com>
2019-03-11 17:02:26 -0500

    add detail and home pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     # route for dogs index
     path('dogs/', views.dogs_index, name='index'),
+    path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Dog:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-dogs = [
-    Dog('Pledge', 'Pitt-Mix', 'tripod', 9),
-    Dog('Brooks', 'Boxer', 'floppy ears', 0),
-    Dog('Swiffer', 'Greyhound ', 'very fast', 4)
-]
+from .models import Dog
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Dogs, Dawgs!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 'dog': dog })
