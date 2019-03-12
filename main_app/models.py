fa305afc95b6193d5b01a1b8fa0b943e62725df8
owner <themichellemcguire@gmail.com>
2019-03-12 17:59:29 -0500

    add feedings

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=100)
@@ -13,3 +19,21 @@ class Dog(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0])
+      # Create a dog_id FK
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+  # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
+from .forms import FeedingForm
 
 # Define the home view
 def home(request):
@@ -30,3 +31,24 @@ class DogUpdate(UpdateView):
 class DogDelete(DeleteView):
   model = Dog
   success_url = '/dogs/'
+
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  # instantiate FeedingForm to be rendered in the template
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', {
+    # pass the dog and feeding_form as context
+    'dog': dog, 'feeding_form': feeding_form
+  })
+
+def add_feeding(request, dog_id):
+  # create the ModelForm using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the dog_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
