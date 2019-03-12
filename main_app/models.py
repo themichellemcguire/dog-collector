0270ecc4db6a2d8ab76902464d049ee5d5a901ee
owner <themichellemcguire@gmail.com>
2019-03-12 12:31:40 -0500

    add update, delete functions

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Dog(models.Model):
@@ -9,3 +10,6 @@ class Dog(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 
 # Define the home view
@@ -15,3 +16,17 @@ def dogs_index(request):
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   return render(request, 'dogs/detail.html', { 'dog': dog })
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = '__all__'
+  success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+  model = Dog
+  # Let's make it impossible to change a dog breed :)
+  fields = ['name', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
