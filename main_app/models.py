a528b5bdf598274a5d32986c7a74a7ef52ec0402
owner <themichellemcguire@gmail.com>
2019-03-13 18:41:29 -0500

    add photos

--- main_app/models.py
+++ main_app/models.py
@@ -8,11 +8,23 @@ MEALS = (
 )
 
 # Create your models here.
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
@@ -37,3 +49,10 @@ class Feeding(models.Model):
 
     class Meta:
         ordering = ['-date']
+
+class Photo(models.Model):
+  url = models.CharField(max_length=200)
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"Photo for dog_id: {self.dog_id} @{self.url}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog
+from django.views.generic import ListView, DetailView
+import uuid
+import boto3
+from .models import Dog, Toy, Photo
 from .forms import FeedingForm
+
+S3_BASE_URL = 'https://s3-us-east-2.amazonaws.com/'
+BUCKET = 'catcollectorlmm'
 
 # Define the home view
 def home(request):
@@ -13,10 +19,6 @@ def about(request):
 def dogs_index(request):
     dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', { 'dogs': dogs })
-
-def dogs_detail(request, dog_id):
-  dog = Dog.objects.get(id=dog_id)
-  return render(request, 'dogs/detail.html', { 'dog': dog })
 
 class DogCreate(CreateView):
   model = Dog
@@ -34,11 +36,13 @@ class DogDelete(DeleteView):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
   # instantiate FeedingForm to be rendered in the template
   feeding_form = FeedingForm()
   return render(request, 'dogs/detail.html', {
-    # pass the dog and feeding_form as context
-    'dog': dog, 'feeding_form': feeding_form
+    'dog': dog, 'feeding_form': feeding_form,
+    # Add the toys to be displayed
+    'toys': toys_dog_doesnt_have
   })
 
 def add_feeding(request, dog_id):
@@ -52,3 +56,46 @@ def add_feeding(request, dog_id):
     new_feeding.dog_id = dog_id
     new_feeding.save()
   return redirect('detail', dog_id=dog_id)
+
+def add_photo(request, dog_id):
+	# photo-file was the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to dog_id or dog (if you have a dog object
+            photo = Photo(url=url, dog_id=dog_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', dog_id=dog_id)  
+
+def assoc_toy(request, dog_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole object
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
